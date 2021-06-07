a7aab15eb9421d5c78b4e204c2699e7bba869062
lycheenut <lycheenut@163.com>
2021-06-07 20:08:06 +0800

    part 8

---
+++ cypress/integration/star.spec.js
@@ -0,0 +1,14 @@
+describe("Test star", () => {
+    before(() => {
+        cy.login()
+    })
+    it("test star", () => {
+        cy.get("[data-unscoped-placeholder=Search or jump to...]").type("awesome-se")
+        cy.get("#jump-to-suggestion-repository-37530945").click()
+        let star = cy.get("[title=Star SJTU-SE/awesome-se]")
+        star.click()
+        star.children("span").contains("Unstar")
+        star.parent().children("a").click()
+        cy.get("[data-hovercard-type=user]").contains("test-account-for-se123")
+    })
+})
