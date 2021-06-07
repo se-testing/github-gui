d404c47b5c15e4a3b0beba90c82d94c21c6ea07d
lycheenut <lycheenut@163.com>
2021-06-07 22:06:39 +0800

    fixed 8

--- cypress/integration/star.spec.js
+++ cypress/integration/star.spec.js
@@ -2,13 +2,21 @@ describe("Test star", () => {
     before(() => {
         cy.login()
     })
-    it("test star", () => {
-        cy.get("[data-unscoped-placeholder|=Search]").type("awesome-se")
+    it("should test star", () => {
+        cy.get("[name=q]").type("awesome-se")
         cy.get("#jump-to-suggestion-repository-37530945").click()
-        let star = cy.get("[title=Star SJTU-SE/awesome-se]")
+        let star = cy.get("body").contains("Star")
         star.click()
-        assert(star.children("span").contains("Unstar"))
+        assert(cy.get("body").contains("Unstar"))
         star.parent().children("a").click()
         assert(cy.get("[data-hovercard-type=user]").contains("test-account-for-se123"))
     })
+    it('should test unstar', () => {
+        cy.visit("https://github.com/SJTU-SE/awesome-se")
+        let unstar = cy.get("body").contains("Unstar")
+        unstar.click()
+        assert(cy.get("body").contains("Star"))
+        unstar.parent().children("a").click()
+        assert(cy.get("[data-hovercard-type=user]").contains("test-account-for-se123").should("not.exist"))
+    });
 })
