60840835690fe7551ccaa8387edff2d55048e1d8
lycheenut <lycheenut@163.com>
2021-06-07 21:36:21 +0800

    fixed 4.1

--- cypress/integration/issue_hotkey.spec.js
+++ cypress/integration/issue_hotkey.spec.js
@@ -6,28 +6,28 @@ describe("Test keyboard shortcut of issue", () => {
         cy.visit("https://github.com/se-testing/repo-for-testing/issues")
     })
     it("test create", () => {
-        cy.get("#js-repo-pjax-container").type('c')
+        cy.get("body").type('c')
         cy.get("#issue_title").should("exist")
     })
     it("test focus searchbar", () => {
-        cy.get("#js-repo-pjax-container").type("{ctrl+/}")
+        cy.get("body").type("{ctrl+/}")
         cy.get("#js-issues-search").should("be.focused")
     })
     it("test author filter", () => {
-        cy.get("#js-repo-pjax-container").type("u")
+        cy.get("body").type("u")
         cy.get("#author-filter-field").should("be.focused")
     })
     it("test label filter", () => {
-        cy.get("#js-repo-pjax-container").type("l")
+        cy.get("body").type("l")
         cy.get("#label-filter-field").should("be.focused")
         cy.type("{alt+enter}")
     })
     it("test milestones filter", () => {
-        cy.get("#js-repo-pjax-container").type("m")
+        cy.get("body").type("m")
         cy.get("#milestones-filter-field").should("be.focused")
     })
     it("test assigns filter", () => {
-        cy.get("#js-repo-pjax-container").type("a")
+        cy.get("body").type("a")
         cy.get("#assigns-filter-field").should("be.focused")
     })
 })
