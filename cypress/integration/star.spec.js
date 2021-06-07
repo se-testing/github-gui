e49b76ffdb21c69219c87427a2346de3564de40f
lycheenut <lycheenut@163.com>
2021-06-07 20:16:48 +0800

    fixed 6

--- cypress/integration/star.spec.js
+++ cypress/integration/star.spec.js
@@ -3,7 +3,7 @@ describe("Test star", () => {
         cy.login()
     })
     it("test star", () => {
-        cy.get("[data-unscoped-placeholder|=Search").type("awesome-se")
+        cy.get("[data-unscoped-placeholder|=Search]").type("awesome-se")
         cy.get("#jump-to-suggestion-repository-37530945").click()
         let star = cy.get("[title=Star SJTU-SE/awesome-se]")
         star.click()
